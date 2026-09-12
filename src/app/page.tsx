ff346570f4a8c2691481e4e03d806cd7796520f9
Yaser Montazeri <76882166+Y0505@users.noreply.github.com>
2026-09-12 21:46:31 +0330

    switch page to v7 interactive factory renderer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { ImmersiveSugarPlant } from "@/components/immersive-sugar-plant-v6";
+import { ImmersiveSugarPlant } from "@/components/immersive-sugar-plant-v7";
 
 const processMap = [
   ["01", "Cane preparation", "Whole cane", "Prepared billets", "Cut & align"],
